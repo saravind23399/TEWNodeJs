c657b2d697af3844eed83faffee7a6e7063b182e
Aravind S <saravind23399@gmail.com>
2019-01-12 21:54:40 +0530

    Initial commit

---
+++ Config/app.config.js
@@ -0,0 +1,13 @@
+config = {
+    database: {
+        name: 'mongodb://admin:tewmepco2019@ds251804.mlab.com:51804/tewmepco'
+    },
+    application: {
+        env: 'development'
+    },
+    pagination: {
+        perPage: 10
+    }
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const config = require('./Config/app.config')
+
