d717355dc9fb3fa2f30d2eb23aba1747b3bbea11
Aravind S <saravind23399@gmail.com>
2019-01-12 22:02:45 +0530

    Initial Commit
    Aravind S

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const express = require('express');
 const config = require('./Config/app.config')
+const cors = require('cors')
+const http = require('http');
+const mongoose = require('mongoose');
 
+
+const port = process.env.PORT || 3000
+var production = false;
+
+
+if (production) {
+    app.use(cors({ origin: 'http://site.to.be.deployed' }));
+} else {
+    app.use(cors({ origin: 'http://localhost:3000' }));
+}
+
+mongoose.connect(config.database.name, { useNewUrlParser: true });
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Database ' + config.database.name);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Database error ' + err);
+});
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('HELLO WORLD!');
+});
+
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
