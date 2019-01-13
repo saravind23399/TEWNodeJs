aec6f3e69d7e159aa1e6e25050777c9ddffd553c
Aravind S <saravind23399@gmail.com>
2019-01-14 00:41:52 +0530

    Added Documentation
    Aravind S

--- Config/app.config.js
+++ Config/app.config.js
@@ -1,13 +1,32 @@
+/*
+    Author : Aravind S
+    Date : 14-JANUARY-2019
+    Description : The master configuration for the Application. 
+    
+                                                *** Change with Care ***
+                            *** Do not change anything if you don't know what you're doing ***
+
+*/
+
+// The Master Configuration Document
 config = {
+
+    // Database Configuration
     database: {
         name: 'mongodb://admin:tewmepco2019@ds251804.mlab.com:51804/tewmepco'
     },
+
+    // Application Configuration
     application: {
         env: 'development'
     },
+
+    // Pagination Configuration
     pagination: {
         perPage: 10
     },
+
+    // Mailing Configuration
     mailCredentials: {
         service: 'gmail',
         auth: {
@@ -17,4 +36,5 @@ config = {
     }
 }
 
+// Exports Master Configuration
 module.exports = config;
